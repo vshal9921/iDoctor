8c2cb877dd5d0255e8b6979c090100a031ec90c6
Vishal Gupta <vishal.gupta@kellton.com>
2024-06-29 20:24:41 +0530

    init Project

---
+++ android/app/src/main/kotlin/com/example/i_doctor/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.i_doctor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:i_doctor/ui/home_widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'iDoctor',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: SafeArea(child: HomeWidget()),
+      )
+    );
+  }
+}
+
