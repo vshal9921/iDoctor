034902da7d45c1a7f47598ec9888e383ffb4f868
Vishal Gupta <vshal9921@gmail.com>
2024-07-01 11:54:02 +0530

    overlap widget done

--- lib/ui/home_widget.dart
+++ lib/ui/home_widget.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:i_doctor/ui/choose_problems.dart';
 import 'package:i_doctor/ui/form.dart';
 import 'top_widget.dart';
@@ -8,15 +10,26 @@ class HomeWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          TopWidget(),
-          RoundedForm(),
-          ChooseProblems()
-        ],
-      )
-    );
+    return Stack(children: [
+      
+      SizedBox(
+        child: Stack(
+          children: [
+            Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  color: Colors.red,
+                  height: 350,
+                  child: const ChooseProblems(),
+                ))
+          ],
+        ),
+      ),
+       const  Positioned(
+          top: 0.0,
+          left: 0,
+          right: 0,
+          child:  RoundedForm()),
+    ]);
   }
 }
-
