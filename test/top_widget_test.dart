086010c2ff6652428eb623eb708fb4e1e4608012
Vishal Gupta <vshal9921@gmail.com>
2024-07-05 14:12:54 +0530

    Widget testing - TopWidget - TextField with inout text

--- test/top_widget_test.dart
+++ test/top_widget_test.dart
@@ -10,7 +10,29 @@ void main(){
         body: TopWidget(),
       ),
     ));
-    var textField = find.byType(Text);
-    expect(textField, findsNWidgets(3));
+
+    var text = find.byType(Text);
+    
+    // Finds multiple widget of type Text.
+    expect(text, findsNWidgets(3));
+  });
+
+  testWidgets('test TextFieild in TopWidget', (widgetTester) async {
+    await widgetTester.pumpWidget(const MaterialApp(
+      home: Scaffold(
+        body: TopWidget(),
+      ),
+    ));
+
+    var textField = find.byType(TextField);
+
+    // finds a single widget of type TextField.
+    expect(textField, findsOneWidget);
+
+    // Enters text for testing
+    await widgetTester.enterText(textField, "Ramesh");
+    // Finds one widget with text "Ramesh"
+    expect(find.text("Ramesh"), findsOneWidget);
+
   });
 }
